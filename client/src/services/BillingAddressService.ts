84b69b4d9eaf0ff6c8077eb18af437867bde3747
Mark Washington <thatbrothacodes@gmail.com>
2020-04-05 01:48:01 -0600

    temporary check-in

--- client/src/services/BillingAddressService.ts
+++ client/src/services/BillingAddressService.ts
@@ -3,16 +3,4 @@ import { BillingAddress } from '../models';
 
 export default class BillingAddressService extends Service<BillingAddress> {
     baseUrl: string = 'billingAddress';
-
-    create = (): Promise<void> => {
-        return super.create();
-    }
-
-    get = async (id: string): Promise<BillingAddress> => {
-        return super.get(id);
-    }
-
-    update = (): Promise<void> => {
-        return super.update();
-    }
 }
---
+++ client/src/services/ShippingAddressService.ts
@@ -0,0 +1,6 @@
+import Service from './';
+import { ShippingAddress } from '../models';
+
+export default class ShippingAddressService extends Service<ShippingAddress> {
+    baseUrl: string = 'shippingAddress';
+}
