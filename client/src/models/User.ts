667a719f85eb736550653cd43f32810f317baba6
Mark Washington <thatbrothacodes@gmail.com>
2020-04-07 20:05:54 -0600

    missed some files

--- client/src/models/User.ts
+++ client/src/models/User.ts
@@ -1,13 +1,15 @@
 export default class User {
     readonly id?: string = undefined;
     email: string;
+    password: string;
     firstName: string;
     middleName?: string;
     lastName: string;
 
-    constructor(id: string, email: string, first: string, middle: string | undefined = undefined, last: string) {
+    constructor(id: string, email: string, password: string, first: string, middle: string | undefined = undefined, last: string) {
         this.id = id;
         this.email = email;
+        this.password = password;
         this.firstName = first;
         this.middleName = middle;
         this.lastName = last;
