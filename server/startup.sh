baa4a98f14aefbf40d667171bad636fbd55d840f
Mark Washington <thatbrothacodes@gmail.com>
2020-04-07 20:04:19 -0600

    finalized initial implementation of migration scripts

--- server/startup.sh
+++ server/startup.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 sleep 20
-# sequelize db:seed:undo:all
+sequelize db:seed:undo:all
 sequelize --env production db:seed --seed 20200407032739-users.js
 sequelize --env production db:seed --seed 20200408011759-products.js
-sleep 60
 yarn start
 
